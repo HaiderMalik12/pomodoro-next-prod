6910379050f3689a0548e21aa25f79e1cb491ec7
Haider Malik <haidermalik504@gmail.com>
2025-08-01 01:10:06 +0500

    setup register form

---
+++ src/app/register/page.tsx
@@ -0,0 +1,10 @@
+import AuthWrapper from "@/components/AuthWrapper";
+import RegisterForm from "@/components/RegisterForm";
+
+export default function Register() {
+  return (
+    <AuthWrapper>
+        <RegisterForm />
+    </AuthWrapper>
+  )
+}
---
+++ src/components/RegisterForm.tsx
@@ -0,0 +1,18 @@
+import * as z from "zod";
+
+const formSchema = z
+  .object({
+    firstName: z.string().min(2, "First name is required"),
+    lastName: z.string().min(2, "Last name is required"),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm password is required"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export default function RegisterForm() {
+  return <div>Register Form</div>;
+}
